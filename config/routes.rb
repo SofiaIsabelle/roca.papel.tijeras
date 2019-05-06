1b916037320f3827cc83a4293a551935bd046011
Sofia Rahimi <sofia.i.rahimi@gmail.com>
2019-05-06 11:36:42 +0000

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-   root 'game#index'
-   get '/play', to: 'game#play'
-   get '/score', to: 'score#index'
-  
-  
+  root 'game#index'
+
+  get '/404', to: 'error#not_found'
+  get '/500', to: 'error#internal_server_error'
+
+  get '/play', to: 'game#play'
+  get '/result/:shape', to: 'game#result', as: :result
+  get '/score', to: 'score#index'
 end
